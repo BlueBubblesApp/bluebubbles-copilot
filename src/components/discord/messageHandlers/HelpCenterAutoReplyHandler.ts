618eccaf0efa6c3396b44f6a10180e25706b9bea
zlshames <zlshames@gmail.com>
2024-06-12 15:15:18 -0400

    adds min length requirement to check messages

--- src/components/discord/messageHandlers/HelpCenterAutoReplyHandler.ts
+++ src/components/discord/messageHandlers/HelpCenterAutoReplyHandler.ts
@@ -5,54 +5,81 @@ import HelpCenterReplies from '../constants/HelpCenterReplies.js';
 const HANDLER_ID = 'help-auto-reply';
 const HANDLER_NAME = 'Helpcenter Auto Reply Handler';
 
-const replyDictionary = {
+type ReplyMap = {
+    [key: string]: {
+        // The regex to test against a message
+        test: RegExp,
+
+        // The reply to send when the test passes
+        reply: string,
+
+        // The minimum number of characters that will match the test.
+        // This allows us to quickly filter out messages that won't match the test.
+        // Regex is expensive, so we want to avoid running it on every message.
+        minLength: number
+    }
+}
+
+const replyDictionary: ReplyMap = {
     cloudflare: {
-        test: new RegExp(/(?:(?:(?:won't)|(?:can'?t)|(?:couldn'?t)|(?:unable to)|(?:failed to)) connect to cloudflare)|(?:(?:(?:can't)|(?:couldn't)|(?:unable to)|(?:failed to)) get cloudflare to work)|(?:cloudflare (?:(?:stopped)|(?:no longer)) work)/gi),
+        test: new RegExp(/(?:(?:(?:won'?t)|(?:can'?t)|(?:couldn'?t)|(?:unable to)|(?:failed to)) connect to cloudflare)|(?:(?:(?:can't)|(?:couldn't)|(?:unable to)|(?:failed to)) get cloudflare to work)|(?:cloudflare (?:(?:stopped)|(?:no longer)) work)/gi),
         reply: HelpCenterReplies.CLOUDFLARE_CONN_HELP,
+        minLength: 23
     },
     noSuchHost: {
         test: new RegExp(/(?:no such host)|(?:(?:failed|unable) to resolve host)/gi),
         reply: HelpCenterReplies.NO_SUCH_HOST_HELP,
+        minLength: 12
     },
     noNotifications: {
         test: new RegExp(/(?:(?:(?:i'?m)|app)(?: (?:is not|isn'?t|not))? (?:receiving|getting) (?:notifications|alerts))/gi),
         reply: HelpCenterReplies.NO_NOTIFICATIONS_HELP,
+        minLength: 21
     },
     howToDonate: {
         test: new RegExp(/how (?:can|do) i (?:donate|support)/gi),
         reply: HelpCenterReplies.HOW_TO_DONATE_HELP,
+        minLength: 15
     },
     bestMacos: {
         test: new RegExp(/(?:(?:best|recommended) mac(?:os)? version)|(?:which mac(?:os)?(?: version)? is(?: the)? (?:best|recommended))/gi),
         reply: HelpCenterReplies.BEST_MACOS_HELP,
+        minLength: 16
     },
     whichProxy: {
         test: new RegExp(/(?:(?:which|what) (?:(?:proxy(?: service)? (?:(?:is(?: the)? best|recommended)|(?:should i use))))|(?: is the (?:best|recommended) (?:proxy service|dynamic dns)))/gi),
         reply: HelpCenterReplies.WHICH_PROXY_HELP,
+        minLength: 18
     },
     portForwarding: {
         test: new RegExp(/(?:how (?:(?:do)|(?:can)) i (?:(?:port forward)|(?:(?:set ?up)|use)(?: a)? dynamic dns))/gi),
         reply: HelpCenterReplies.PORT_FORWARDING_HELP,
+        minLength: 21
     },
     whyNeedMac: {
         test: new RegExp(/(?:(?:why|do i need)(?: a)? mac)/gi),
         reply: HelpCenterReplies.WHY_NEED_MAC_HELP,
+        minLength: 13
     },
     howToGetPapiFeatures: {
         test: new RegExp(/(?:(?:how|where) (?:can|do) i (?:get|use|(?:set ?up)) (?:reactions|tapbacks|replies|edit|unsend))/gi),
         reply: HelpCenterReplies.HOW_TO_GET_PAPI_FEATURES_HELP,
+        minLength: 20
     },
     appleScriptReboot: {
         test: new RegExp(/(?:apple ?event timed? out)|(?:can't get (?:(?:buddy)|(?:participant)|(?:chat)) id)/gi),
         reply: HelpCenterReplies.APPLE_SCRIPT_REBOOT_HELP,
+        minLength: 19
     },
     appleScriptEvents: {
         test: new RegExp(/not authorized to send (?:(?:apple)|(?:system)) events/gi),
         reply: HelpCenterReplies.APPLE_SCRIPT_EVENTS_HELP,
+        minLength: 35
     },
     bbOnIos: {
-        test: new RegExp(/bluebubbles work on(?: an)?(?: old)? (?:(?:ipad)|(?:iphone))/gi),
+        test: new RegExp(/bluebubbles work on(?: an)?(?: old)? (?:(?:ipad)|(?:iphone)|(?:ios))/gi),
         reply: HelpCenterReplies.BB_ON_IOS_HELP,
+        minLength: 23
     },
 };
 
@@ -97,6 +124,9 @@ export default class HelpCenterAutoReplyHandler extends BaseMessageHandler {
 
     async checkForAutoReply(message: Message<boolean>): Promise<void> {
         for (const [key, value] of Object.entries(replyDictionary)) {
+            // Check the min length required to do the test.
+            // This allows us to quickly filter out messages we won't find matching strings in.
+            if (message.content.length < value.minLength) continue;
             if (!message.content.match(value.test)) continue;
 
             // If we've already replied to this user in the last 24 hours, don't reply again
